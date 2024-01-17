c60132685e1b1fff075b8436ca0c3358cbe90bde
Kevin Orel Edry <techg9@gmail.com>
2024-01-17 13:03:19 -0800

    added roboto fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,26 @@
     import Typewriter from 'svelte-typewriter'
 </script>
 
-<div>
-    <Typewriter mode="loop" cursor={false} interval={100} delay={5000}>
-        <h1>Hello, I'm Kevin.</h1>
-        <h1>नमस्ते, मैं केविन हूं।</h1>
-        <h1>你好，我是凯文。</h1>
-        <h1>Hola, soy Kevin.</h1>
-        <h1>こんにちは、ケビンです。</h1>
-        <h1>אהלן, אני קווין.</h1>
-    </Typewriter>
-    <slot></slot>
+<div class="flex flex-row font-roboto">
+    <nav class="w-1/12">
+        <ul>
+            <li>about</li>
+            <li>projects</li>
+            <li>blog</li>
+            <li>contact</li>
+        </ul>
+    </nav>
+    <section>
+        <Typewriter mode="loop" cursor={false} interval={100} delay={5000}>
+            <h1>Hello, I'm Kevin.</h1>
+            <h1>नमस्ते, मैं केविन हूं।</h1>
+            <h1>你好，我是凯文。</h1>
+            <h1>Hola, soy Kevin.</h1>
+            <h1>こんにちは、ケビンです。</h1>
+            <h1>אהלן, אני קווין.</h1>
+        </Typewriter>
+        <slot></slot>
+    </section>
     <div>
 
     </div>
