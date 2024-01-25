472b561871e1400bbab6d2c7eb96bae45a279182
Kevin Orel Edry <techg9@gmail.com>
2024-01-24 20:44:26 -0800

    messeging update

--- src/components/page-transition-layout/page-transition-layout.tsx
+++ src/components/page-transition-layout/page-transition-layout.tsx
@@ -81,18 +81,25 @@ export default function PageTransitionLayout({
           )}
         </LayoutGroup>
       </div>
-      <div className="container mx-auto flex h-full flex-col flex-nowrap items-center justify-center gap-3 overflow-hidden p-10 font-roboto text-text sm:hidden">
-        <h1 className="mt-auto text-6xl font-extrabold">Please...</h1>
+      <div className="container mx-auto flex h-full flex-col flex-nowrap items-center justify-center gap-3 overflow-hidden p-5 font-roboto text-text sm:hidden">
+        <h1 className="mt-auto text-center text-3xl">
+          <span className="font-extrabold">
+            Leading with innovation,
+            <br />
+          </span>
+          not just implementation
+        </h1>
         <hr className="w-32 text-primary" />
-        <p className="text-center text-3xl">
-          I’m an
-          <span className="font-bold"> Engineering Manager</span>, Do I look
-          like I spend my days tweaking CSS?
+        <p className="text-center text-lg">
+          As an
+          <span className="font-bold"> Engineering Manager</span>, I prioritize
+          strategic leadership and team development over pixel-perfect layouts.
         </p>
-        <div className="text-6xl">😂</div>
+        <div className="text-center text-text/70"></div>
+        <div className="text-6xl">😎</div>
         <div className="mt-auto text-center text-text/70">
-          I just didn&apos;t have the time to implement it, I actually like
-          tweaking CSS all day.
+          A responsive design update is in the pipeline as I continuously
+          improve the website. Stay tuned!
         </div>
       </div>
     </React.Fragment>
