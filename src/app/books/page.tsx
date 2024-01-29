ed70eb1c261e35fe206354224d7e1616c9fb2ad6
Kevin Orel Edry <techg9@gmail.com>
2024-01-28 17:24:29 -0800

    add book covers to images

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 export default function Books() {
   return (
     <React.Fragment>
-      <div className="flex h-[80%] w-1/2 flex-col gap-3"></div>
+      <div className="flex h-[80%] w-1/2 flex-col gap-3">
+        <ul>
+          <li>
+            <Image />
+          </li>
+        </ul>
+      </div>
       <div className="flex h-[80%] w-1/2 flex-col gap-4"></div>
     </React.Fragment>
   );
