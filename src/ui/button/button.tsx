55138262e9ba37a325af1b53f4c2f429385d030f
Kevin Orel Edry <techg9@gmail.com>
2024-01-28 17:33:20 -0800

    added onClick event handling to button

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -1,12 +1,13 @@
 export default function Button(props: {
-  onClick?: (e: React.MouseEventHandler<HTMLButtonElement>) => {
-    //This is empty because this will be implemented by the user of this component
-  };
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }) {
   const { onClick, children } = props;
   return (
-    <button className="h-10 w-32 rounded-md bg-primary font-bold hover:bg-primary/90">
+    <button
+      onClick={onClick}
+      className="h-10 w-32 rounded-md bg-primary font-bold hover:bg-primary/90"
+    >
       {children}
     </button>
   );
